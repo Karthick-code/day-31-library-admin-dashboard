f1d473b3506dc12f80d2b7baf2586254b66a0d7d
Karthick-code <karthick02122000@gmail.com>
2024-08-20 21:06:59 +0530

    removing unwanted comments

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -41,33 +41,3 @@ export default Dashboard;
 
 
 
-
-
-// // src/components/Dashboard.jsx
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Container, Grid, Paper, Typography } from '@mui/material';
-
-// const Dashboard = () => {
-//   return (
-//     <Container>
-//       <Typography variant="h4" component="h1" gutterBottom>
-//         Library Admin Dashboard
-//       </Typography>
-//       <Grid container spacing={3}>
-//         <Grid item xs={12} md={6}>
-//           <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
-//             <Link to="/books">Manage Books</Link>
-//           </Paper>
-//         </Grid>
-//         <Grid item xs={12} md={6}>
-//           <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
-//             <Link to="/authors">Manage Authors</Link>
-//           </Paper>
-//         </Grid>
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default Dashboard;
